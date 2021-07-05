011b6f1b2ea68916ff50c58afe78e744ead0b2c3
ConnerMcB <connermcb@yahoo.com>
2021-07-05 11:05:33 -0500

    add user input validation logic

--- src/model/RoomType.java
+++ src/model/RoomType.java
@@ -1,6 +1,23 @@
 package model;
 
+import java.util.List;
+import java.lang.String;
+
 public enum RoomType {
     SINGLE,
-    DOUBLE
+    DOUBLE;
+
+//    Code pattern for checking if value in RoomType Enum adapted from:
+//    https://www.linuxcommands.site/java/java-tutorial-java-enum-check-if-value-exists/
+    public static Boolean isValid(String userInput) {
+        if (userInput.isEmpty()) {
+            return false;
+        }
+        for (RoomType each : RoomType.values()) {
+            if (userInput.equals(each.name())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
